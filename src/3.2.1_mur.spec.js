ea3ec2ca6c8d8d039d4cb3184374701dad8caa6a
Florent Perinel <florent.perinel@redfroggy.fr>
2024-06-03 15:28:19 +0200

    fix: bad mur0 value

--- src/3.2.1_mur.spec.js
+++ src/3.2.1_mur.spec.js
@@ -4,7 +4,7 @@ describe('Recherche de bugs dans le calcul de déperdition des murs', () => {
   /**
    * @see : https://redfroggy.atlassian.net/browse/KAR-119
    */
-  test('calcul de déperdition pour les murs de 2213E0696993Z', () => {
+  it('calcul de déperdition pour les murs de 2213E0696993Z', () => {
     const zc = 8; // H3
     const pc_id = 2; // Période de construction (1948)
     const ej = 0;
@@ -42,5 +42,79 @@ describe('Recherche de bugs dans le calcul de déperdition des murs', () => {
     expect(mur.donnee_intermediaire.b).toBe(0.35);
     expect(mur.donnee_intermediaire.umur).toBe(2.5);
     expect(mur.donnee_intermediaire.umur0).toBe(2.5);
+  });
+
+  /**
+   * @see : https://redfroggy.atlassian.net/browse/KAR-118
+   */
+  xit('calcul de déperdition pour les murs de 2187E0982013C', () => {
+    const zc1 = 3; // zone climatique H1c
+    const pc_id1 = 1; // Période de construction (1947)
+    const ej1 = 1; // Type de chauffage
+    const mur1 = {
+      donnee_entree: {
+        description:
+          "Mur Nord, Sud, Est, Ouest - Mur en pierre de taille et moellons avec remplissage tout venant d'épaisseur 50 cm non isolé donnant sur l'extérieur",
+        enum_type_adjacence_id: '1', // Extérieur
+        enum_orientation_id: '4', // Sud Ouest
+        surface_paroi_totale: 134.76,
+        surface_paroi_opaque: 134.76,
+        tv_umur0_id: 15,
+        enum_materiaux_structure_mur_id: '3', // Mur en pierre de taille et moellons avec remplissage tout venant
+        enum_methode_saisie_u0_id: '2', // déterminé selon le matériau et épaisseur à partir de la table de valeur forfaitaire
+        paroi_ancienne: 1,
+        enum_type_doublage_id: '2', // absence de doublage
+        enum_type_isolation_id: '2', // Non isolé
+        enum_methode_saisie_u_id: '1', // Non isolé
+        reference: 'mur_0'
+      },
+      donnee_intermediaire: {
+        b: 1,
+        umur: 0.81545, // uMur-tab 1947 sur zc H1 = 2.5
+        umur0: 0.81545
+      }
+    };
+    calc_mur(mur1, zc1, pc_id1, ej1);
+
+    expect(mur1.donnee_intermediaire.b).toBe(1);
+    expect(mur1.donnee_intermediaire.umur).toBe(1.9);
+    expect(mur1.donnee_intermediaire.umur0).toBe(1.9);
+  });
+
+  /**
+   * @see : https://redfroggy.atlassian.net/browse/KAR-123
+   */
+  it('calcul de déperdition pour les murs de 2287E1327399F', () => {
+    const zc = 3;
+    const pc_id = 1;
+    const ej = 1;
+    const mur = {
+      donnee_entree: {
+        description:
+          "Mur  1 Nord - Mur en pierre de taille et moellons avec remplissage tout venant d'épaisseur 60 cm avec isolation intérieure (R=3,7m².K/W) donnant sur un bâtiment ou local à usage autre que d'habitation",
+        reference: '2022_06_15_14_48_45_6578917004465132',
+        tv_coef_reduction_deperdition_id: 4,
+        enum_type_adjacence_id: '4',
+        enum_orientation_id: '2',
+        surface_paroi_totale: 48.39,
+        surface_paroi_opaque: 48.39,
+        tv_umur0_id: 17,
+        epaisseur_structure: 60,
+        enum_materiaux_structure_mur_id: '3',
+        enum_methode_saisie_u0_id: '2',
+        paroi_ancienne: 1,
+        enum_type_doublage_id: '2',
+        enum_type_isolation_id: '3',
+        resistance_isolation: 3.7,
+        enum_methode_saisie_u_id: '6'
+      },
+      donnee_intermediaire: { b: 0.2, umur: 0.19900518501955455, umur0: 0.7547200000000001 }
+    };
+    calc_mur(mur, zc, pc_id, ej);
+
+    // Est-ce que ces valeurs sont correctes ? le umurO en particulier ne correspond à rien (d'apr-s la doc c'est 1.6)
+    expect(mur.donnee_intermediaire.b).toBe(0.2);
+    expect(mur.donnee_intermediaire.umur0).toBeCloseTo(0.7547200000000001);
+    expect(mur.donnee_intermediaire.umur).toBeCloseTo(0.19900518501955455);
   });
 });
